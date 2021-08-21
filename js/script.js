e5ebb0eb30edb597eda1d1084facc48fc618d587
Shariful-Isla-Sourav <sorabsorab347@gmail.com>
2021-08-21 17:10:53 +0600

    Release v1.0

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-//Update item Cost
-function updateVariant(initCost, newCost) {
+//Update Each item Cost
+function updateVariantCost(initCost, newCost) {
   const itemCost = document.getElementById(initCost);
   itemCost.innerText = newCost;
   return itemCost.innerText;
 }
 
-//Update total cost
+//Update total  & grand total
 function updateTotalCost() {
   const ramPrice = parseFloat(document.getElementById("memory-cost").innerText);
   const ssdPrice = parseFloat(
@@ -20,52 +20,52 @@ function updateTotalCost() {
   //Update grand total
   document.getElementById("grand-total").innerText = totalCost;
 }
-//Ram Price Update
+//Ram Price Handler
 document.getElementById("ram-8gb-btn").addEventListener("click", function () {
-  updateVariant("memory-cost", 0);
+  updateVariantCost("memory-cost", 0);
   updateTotalCost();
 });
 document
   .getElementById("ram-16gb-btn")
   .addEventListener("click", function ido() {
-    updateVariant("memory-cost", 180);
+    updateVariantCost("memory-cost", 180);
     updateTotalCost();
   });
-//SSD Price Update
+//SSD Price Handler
 document
   .getElementById("storage-256gb-btn")
   .addEventListener("click", function () {
-    updateVariant("storage-cost", 0);
+    updateVariantCost("storage-cost", 0);
     updateTotalCost();
   });
 document
   .getElementById("storage-512gb-btn")
   .addEventListener("click", function () {
-    updateVariant("storage-cost", 100);
+    updateVariantCost("storage-cost", 100);
     updateTotalCost();
   });
 document
   .getElementById("storage-1tb-btn")
   .addEventListener("click", function () {
-    updateVariant("storage-cost", 180);
+    updateVariantCost("storage-cost", 180);
     updateTotalCost();
   });
 
-//Delivery Price Update
+//Delivery Price Handler
 document
   .getElementById("free-delivery-btn")
   .addEventListener("click", function () {
-    updateVariant("delivery-cost", 0);
+    updateVariantCost("delivery-cost", 0);
     updateTotalCost();
   });
 document
   .getElementById("paid-delivery-btn")
   .addEventListener("click", function () {
-    updateVariant("delivery-cost", 20);
+    updateVariantCost("delivery-cost", 20);
     updateTotalCost();
   });
 
-//Coupon Offer Update
+//Coupon Offer Update Handler
 document.getElementById("coupon-btn").addEventListener("click", function () {
   const previousTotalCost = document.getElementById("total-cost").innerText;
   const coupon = document.getElementById("coupon-code");
@@ -77,3 +77,5 @@ document.getElementById("coupon-btn").addEventListener("click", function () {
     coupon.value = "";
   }
 });
+
+// ------------------------END OF CODE---------------------------
